bae1c05896384c1186e0eb4123aafe79bf55c5d9
vadinamo <vadinamo@gmail.com>
2022-11-12 01:53:52 +0300

    lab5 hotfix

--- WEB_053502_YUREV/WEB_053502_YUREV/Controllers/ProductController.cs
+++ WEB_053502_YUREV/WEB_053502_YUREV/Controllers/ProductController.cs
@@ -1,138 +1,33 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using WEB_053502_YUREV.Data;
 using WEB_053502_YUREV.Entities;
 using WEB_053502_YUREV.Models;
 
-namespace WEB_053502_YUREV.Controllers
+namespace WEB_053502_YUREV.Controllers;
+
+public class ProductController : Controller
 {
-    public class ProductController : Controller
+     ApplicationDbContext _context;
+    int _pageSize;
+        
+    public ProductController(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
-        private readonly IWebHostEnvironment _hostEnvironment;
-        int _pageSize;
-        [BindProperty] public InputModel Input { get; set; }
+        _pageSize = 3;
+        _context = context;
+    }
+
+    [Route("Catalog")]
+    [Route("Catalog/Page_{page=1}")]
+    public IActionResult Index(Guid? category, int page)
+    {
+        var filteredItems = _context.Items
+            .Where(i => !category.HasValue || i.CategoryId == category.Value);
         
-        public ProductController(ApplicationDbContext context, IWebHostEnvironment hostEnvironment)
-        {
-            _pageSize = 3;
-            _context = context;
-            _hostEnvironment = hostEnvironment;
-        }
-        
-        // GET: Product
-        [Route("Catalog")]
-        [Route("Catalog/Page_{pageNo=1}")]
-        public async Task<IActionResult> Index(Guid category, int pageNo)
-        {
-            var filteredItems = _context.Items.Where(i => category == Guid.Empty || i.CategoryId == category);
-        
-            ViewData["Groups"] = _context.ItemCategories;
-            var currentGroup = category != Guid.Empty ? category : Guid.Empty;
-            ViewData["CurrentGroup"] = currentGroup;
+        ViewData["Categories"] = _context.ItemCategories;
+        ViewData["CurrentCategory"] = category ?? Guid.Empty;
             
-            return _context.Items != null ? 
-                View(ListViewModel<Item>.GetModel(filteredItems, pageNo, _pageSize)) :
-                Problem("Entity set is null.");
-        }
-
-        // GET: Product/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-        
-        // GET: Product/Create
-        public IActionResult Create()
-        {
-            ViewData["Categories"] = _context.ItemCategories.ToList();
-            return View();
-        }
-        
-        // POST: Product/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Description,Category,Price,Image")] Item item)
-        {
-            
-            if (ModelState.IsValid)
-            {
-                item.Id = Guid.NewGuid();
-                if (Input.ImageUpload != null)
-                {
-                    using (var stream = new FileStream(Path.Combine(_hostEnvironment.WebRootPath, "cars",
-                               item.Id.ToString("N")), FileMode.Create))
-                    {
-                        await Input.ImageUpload.FormFile.CopyToAsync(stream);
-                    }
-
-                    item.Image = item.Id.ToString("N") + Input.ImageUpload.FormFile.FileName.Split('.')[1];
-                }
-                
-                item.Category = _context.ItemCategories.
-                    FirstOrDefaultAsync(m => m.Id == item.Id).Result;
-
-                _context.Add(item);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(item);
-        }
-        
-        // GET: Product/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-        
-        // POST: Product/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-        
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-        
-        // GET: Product/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-        
-        // POST: Product/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-        
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-        
-        public class InputModel
-        {
-            [BindProperty] public FileUpload ImageUpload { get; set; }
-        }
+        return _context.Items != null ? 
+            View(ListViewModel<Item>.GetModel(filteredItems, page, _pageSize)) :
+            Problem("Entity set is null.");
     }
 }
---
+++ WEB_053502_YUREV/WEB_053502_YUREV/TagHelpers/PagerTagHelper.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WEB_053502_YUREV.TagHelpers;
+
+public class PagerTagHelper : TagHelper
+{
+    LinkGenerator _linkGenerator;
+
+    public int PageCurrent { get; set; }
+
+    public int PageTotal { get; set; }
+
+    public string PagerClass { get; set; }
+
+    public string Action { get; set; }
+
+    public string Controller { get; set; }
+    public Guid? GroupId { get; set; }
+
+    public PagerTagHelper(LinkGenerator linkGenerator)
+    {
+        _linkGenerator = linkGenerator;
+    }
+
+    public override void Process(TagHelperContext context,
+        TagHelperOutput output)
+
+    {
+        output.TagName = "nav";
+        var ulTag = new TagBuilder("ul");
+        ulTag.AddCssClass("pagination");
+        ulTag.AddCssClass(PagerClass);
+        for (int i = 1; i <= PageTotal; i++)
+        {
+            var url = _linkGenerator.GetPathByAction(Action, Controller,
+                new
+                {
+                    page = i,
+                    group = GroupId == Guid.Empty
+                        ? null
+                        : GroupId
+                });
+
+            var item = GetPagerItem(
+                url: url,
+                text: i.ToString(),
+                active: i == PageCurrent,
+                disabled: i == PageCurrent
+            );
+            ulTag.InnerHtml.AppendHtml(item);
+        }
+
+        output.Content.AppendHtml(ulTag);
+    }
+    
+    private TagBuilder GetPagerItem(string url, string text,
+        bool active = false,
+        bool disabled = false)
+
+    {
+        var liTag = new TagBuilder("li");
+        liTag.AddCssClass("page-item");
+        liTag.AddCssClass(active ? "active" : "");
+        //liTag.AddCssClass(disabled ? "disabled" : "");
+        var aTag = new TagBuilder("a");
+        aTag.AddCssClass("page-link");
+        aTag.Attributes.Add("href", url);
+        aTag.InnerHtml.Append(text);
+        liTag.InnerHtml.AppendHtml(aTag);
+        return liTag;
+    }
+}
