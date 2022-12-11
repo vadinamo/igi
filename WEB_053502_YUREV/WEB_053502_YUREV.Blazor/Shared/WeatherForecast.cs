0d24b13df487a5d28bc012888d28854b03649fa5
vadinamo <vadinamo@gmail.com>
2022-12-12 01:31:58 +0300

    lab8 task 3

---
+++ WEB_053502_YUREV/WEB_053502_YUREV.Blazor/Shared/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace WEB_053502_YUREV.Blazor.Shared;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public string? Summary { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
